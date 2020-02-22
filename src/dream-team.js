27d453f7914d8b869ffadcefa636e837dc99014f
Sergey Tokar <tokarsergey1988@gmail.com>
2020-02-22 17:18:35 +0300

    feat: add solution for dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeamName = "";
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  members.forEach(element => {
+    if (typeof element == "string") {
+      element = element.split('');
+      element = element.filter(item => { return item != " " });
+      dreamTeamName = dreamTeamName + element[0].toUpperCase();
+    };
+  });
+
+  dreamTeamName = dreamTeamName.split('').sort().join('');
+
+  return dreamTeamName;
 };
