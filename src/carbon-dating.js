c40d6fa8f3dbb6734c0e4fbcbd8e848b94857031
Sergey Tokar <tokarsergey1988@gmail.com>
2020-02-22 16:05:54 +0300

    feat: add solution for carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string") {
+    return false;
+  }
+
+  sampleActivity = parseFloat(sampleActivity);
+
+  if (!sampleActivity || sampleActivity > 15 || sampleActivity <= 0) {
+    return false;
+  };
+
+  let age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  return age;
 };
